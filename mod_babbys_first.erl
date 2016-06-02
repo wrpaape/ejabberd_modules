e2c59411cd68bfe5cc17fd32f3801877c7f7006a
Reid Paape <wrpaape@gmail.com>
2016-06-02 16:58:07 -0500

    moving up a directory

---
+++ mod_babbys_first.erl
@@ -0,0 +1,19 @@
+-module(mod_babbys_first).
+
+-behaviour(gen_mod).
+
+-include("logger.hrl").
+
+-export([start/2,
+         stop/1]).
+
+
+
+start(_Host, _Opt) ->
+  ?INFO_MSG("starting module 'mod_babbys_first'", []),
+  ok.
+
+
+stop(_Host) ->
+  ?INFO_MSG("stopping module 'mod_babbys_first'", []),
+  ok.
