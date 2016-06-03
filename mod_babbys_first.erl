ca1aa238ebc95fc2eede6b17c3c77c6158aa3f19
Reid Paape <wrpaape@gmail.com>
2016-06-03 06:28:43 -0500

    attempting simple http auth

--- mod_babbys_first.erl
+++ mod_babbys_first.erl
@@ -4,9 +4,11 @@
 
 -include("ejabberd.hrl").
 -include("logger.hrl").
+-include("ejabberd_http.hrl").
 
 -export([start/2,
          stop/1,
+         process/2,
          mod_opt_type/1]).
 
 
@@ -21,9 +23,40 @@ stop(_Host) ->
   ok.
 
 
+process(Path, _Request) ->
+  {xmlelement,
+   "html",
+   [{"xmlns",
+     "http://www.w3.org/1999/xhtml"}],
+   [{xmlelement,
+     "head",
+     [],
+     [{xmlelement,
+       "title",
+       [],
+       []}]},
+    {xmlelement,
+     "body",
+     [],
+     [{xmlelement,
+       "p",
+       [],
+       [{xmlcdata,
+         is_user_exists(Path)}]}]}]}.
+
+
+is_user_exists(User) ->
+  Result = ejabberd_auth:is_user_exists(User, "localhost"),
+  case Result of
+    true  -> "The username " ++ User ++ " is already taken.";
+    false -> "The username " ++ User ++ " is available."
+  end.
+
+
 mod_opt_type(cache_life_time) -> fun (I) when is_integer(I), I > 0 -> I end;
 
 mod_opt_type(cache_size)      -> fun (I) when is_integer(I), I > 0 -> I end;
+
 mod_opt_type(db_type)         -> fun gen_mod:v_db/1;
 
 mod_opt_type(default)         -> fun (always) -> always;
